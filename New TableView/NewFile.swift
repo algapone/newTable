8e6ac89a362e05f3eca25f3618a8ba6aa144a666
Dmitry <79645198490@yandex.ru>
2019-12-12 00:12:39 +0300

    second repo

--- New TableView/NewFile.swift
+++ New TableView/NewFile.swift
@@ -14,8 +14,65 @@ class NewFile: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Testprint")
+        initProjectOne()
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func initProjectOne() {
+        // https://jsonplaceholder.typicode.com/
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users")
+            else { return }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url) { (data, response, error)
+            in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+            }
+            
+            if let error = error {
+                print("error")
+            } else if let data = data {
+                print(data)
+                
+//                let dataString = String(data: data, encoding: .utf8)
+//                print(dataString)
+            
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    
+                } catch let err {
+                    print(err)
+                }
+                
+            
+            }
+        }
+        
+        dataTask.resume()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return interConnect.count
